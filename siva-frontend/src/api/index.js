490f079a8e7e4b7434f595cb4e93da327fefec77
Siva <sivasow2024@gmail.com>
2026-09-21 12:11:37 +0530

    Point frontend to live backend URL

--- siva-frontend/src/api/index.js
+++ siva-frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:8080";
+const BACKEND_URL = "https://makemytrip-application.onrender.com";
 
 export const login = async (email, password) => {
   try {
